13c4beece4c4e1636d7355c95599d0101ae8e7b4
Henry H. Hank <trahnagrom@live.co.uk>
2014-03-25 10:53:38 -0400

    Âdone triangles, halfway through minmax

---
+++ triangletests.py
@@ -0,0 +1,16 @@
+import triangle
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+
+
+    def test1(self):
+        assert triangle.area(3,2) == 3 #test 1 fails
+        
+    def test2(self):
+        assert triangle.area(4,3) == 6 #test 2 fails
+     
+
+if __name__ == '__main__':
+    unittest.main()
